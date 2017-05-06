80ee2bf450ad3c929f6de7428671a0a5c274f3f6
persic <cpersich@uci.edu>
2017-05-05 22:47:23 -0700

    Cleaned up the plotting scripts to make them more modular

---
+++ plottingScripts/plotVeffUtils.py
@@ -0,0 +1,121 @@
+import os, sys, ROOT, numpy as np, matplotlib.pyplot as plt
+
+def DictToArray(Dict):
+	outArray=np.array(Dict.items())
+	outArray=np.sort(outArray,axis=0)
+	return outArray
+
+def CalcVeffMutiLog(infn,Tname="nt"):
+	inf=ROOT.TFile(infn)
+	nt=inf.Get(Tname)
+	Values=[]
+	for ent in nt:
+		E=ent.energy
+		VeffAve=ent.VeffAve
+		VeffNuE=ent.VeffNuE
+		VeffNuMu=ent.VeffNuMu
+		VeffNuTau=ent.VeffNuTau
+		WtNuE=ent.wtNuE
+		WtNuMu=ent.wtNuMu
+		WtNuTau=ent.wtNuTau
+		WtAve=(WtNuE+WtNuMu+WtNuTau)/3
+		Values.append((E,VeffAve,VeffNuE,VeffNuMu,VeffNuTau,WtAve,WtNuE,WtNuMu,WtNuTau))
+	VvsEAve={}
+	VvsENuE={}
+	VvsENuMu={}
+	VvsENuTau={}
+	ErrVsEAve={}
+	ErrVsENuE={}
+	ErrVsENuMu={}
+	ErrVsENuTau={}
+	for (E,VeffAve,VeffNuE,VeffNuMu,VeffNuTau,WtAve,WtNuE,WtNuMu,WtNuTau) in Values:
+		if E in VvsEAve:
+			#First do a weighted sum of Veff for each energy
+			VvsEAve[E]+=VeffAve*WtAve
+			VvsENuE[E]+=VeffNuE*WtNuE
+			VvsENuMu[E]+=VeffNuMu*WtNuMu
+			VvsENuTau[E]+=VeffNuTau*WtNuTau
+			#First sum all the weights
+			ErrVsEAve[E]+=WtAve
+			ErrVsENuE[E]+=WtNuE
+			ErrVsENuMu[E]+=WtNuMu
+			ErrVsENuTau[E]+=WtNuTau
+		else:
+			#First do a weighted sum of Veff for each energy
+			VvsEAve[E]=VeffAve*WtAve
+			VvsENuE[E]=VeffNuE*WtNuE
+			VvsENuMu[E]=VeffNuMu*WtNuMu
+			VvsENuTau[E]=VeffNuTau*WtNuTau
+			#First sum all the weight
+			ErrVsEAve[E]=WtAve
+			ErrVsENuE[E]=WtNuE
+			ErrVsENuMu[E]=WtNuMu
+			ErrVsENuTau[E]=WtNuTau
+
+	for E in VvsEAve:
+		if ErrVsEAve[E] !=0 :
+			#divide the weighted sum by the total weight to get the weighted average
+			VvsEAve[E]=VvsEAve[E]/ErrVsEAve[E]
+			#StdErr = mean/sqrt(N)
+			ErrVsEAve[E]=VvsEAve[E]/np.sqrt(ErrVsEAve[E])
+		else:
+			VvsEAve[E]=0
+
+		if ErrVsENuE[E] != 0:
+			VvsENuE[E]=VvsENuE[E]/ErrVsENuE[E]
+			ErrVsENuE[E]=VvsENuE[E]/np.sqrt(ErrVsENuE[E])
+		else:
+			VvsENuE[E]=0
+
+		if ErrVsENuMu[E] != 0:
+			VvsENuMu[E]=VvsENuMu[E]/ErrVsENuMu[E]
+			ErrVsENuMu[E]=VvsENuMu[E]/np.sqrt(ErrVsENuMu[E])
+		else:
+			VvsENuMu[E]=0
+
+		if ErrVsENuTau[E] != 0:
+			VvsENuTau[E]=VvsENuTau[E]/ErrVsENuTau[E]
+			ErrVsENuTau[E]=VvsENuTau[E]/np.sqrt(ErrVsENuTau[E])
+		else:
+			VvsENuTau[E]=0
+
+	VvsEAve=DictToArray(VvsEAve)
+	VvsENuE=DictToArray(VvsENuE)
+	VvsENuMu=DictToArray(VvsENuMu)
+	VvsENuTau=DictToArray(VvsENuTau)
+	ErrVsEAve=DictToArray(ErrVsEAve)
+	ErrVsENuE=DictToArray(ErrVsENuE)
+	ErrVsENuMu=DictToArray(ErrVsENuMu)
+	ErrVsENuTau=DictToArray(ErrVsENuTau)
+
+	OutArray=np.column_stack((VvsEAve[:,0],VvsEAve[:,1],VvsENuE[:,1],VvsENuMu[:,1],VvsENuTau[:,1],ErrVsEAve[:,1],ErrVsENuE[:,1],ErrVsENuMu[:,1],ErrVsENuTau[:,1]))
+	#names = ['E', 'VeffAve', 'VeffNuE', 'VeffNuMu', 'VeffNuTau', 'VeffErrAve', 'VeffErrNuE', 'VeffErrNuMu', 'VeffErrNuTau']
+	return OutArray
+
+def GetColumn(DataArray,name):
+	names = ['E', 'VeffAve', 'VeffNuE', 'VeffNuMu', 'VeffNuTau', 'VeffErrAve', 'VeffErrNuE', 'VeffErrNuMu', 'VeffErrNuTau']
+	i=names.index(name)
+	Column=DataArray[:,i]
+	return Column
+
+def RatiosWithErrors(Numerator,Denominator,nError,dError):
+	Ratio=Numerator/d_AT2ST_denominator
+	RatioErrHi=(Numerator+nError)/(Denominator-dError) - Ratio
+	RatioErrLo=Ratio - (Numerator-nError)/(Denominator+dError)
+	return Ratio, RatioErrHi, RatioErrLo
+
+def GetAllColumns(DataArray):
+	E=GetColumn(DataArray,'E')
+	VeffAve=GetColumn(DataArray,'VeffAve')
+	VeffNuE=GetColumn(DataArray,'VeffNuE')
+	VeffNuMu=GetColumn(DataArray,'VeffNuMu')
+	VeffNuTau=GetColumn(DataArray,'VeffNuTau')
+	VeffErrAve=GetColumn(DataArray,'VeffErrAve')
+	VeffErrNuE=GetColumn(DataArray,'VeffErrNuE')
+	VeffErrNuMu=GetColumn(DataArray,'VeffErrNuMu')
+	VeffErrNuTau=GetColumn(DataArray,'VeffErrNuTau')
+	return E, VeffAve, VeffNuE, VeffNuMu, VeffNuTau, VeffErrAve, VeffErrNuE, VeffErrNuMu, VeffErrNuTau
+
+def SaveCSV(DataArray,outfn):
+	names = ['E', 'VeffAve', 'VeffNuE', 'VeffNuMu', 'VeffNuTau', 'VeffErrAve', 'VeffErrNuE', 'VeffErrNuMu', 'VeffErrNuTau']
+	nn.savetxt(outfn,outArray,delimiter=",",header=names)
--- plottingScripts/plotVeff_example.py
+++ plottingScripts/plotVeff_example.py
@@ -1,153 +1,53 @@
 import os, sys, ROOT, numpy as n, matplotlib.pyplot as plt
+from plotVeffUtils import *
 
-#title=sys.argv[1]
 title="Moores bay vs SP"
 fileheader=title.replace(" ","_")
+infNames=["ParsedLogs_Example/VeffCombo_3Sig_MB_HProp500_ST6_parsed.root","ParsedLogs_Example/VeffCombo_3Sig_SP_1400m_HProp500_ST6_parsed.root"]
+labels=["Moore's Bay","South Pole"]
 
-#infNames=["PointOneMeterR_parsed.root","FourMeterR_parsed.root","TenMeterR_parsed.root","ThirtyMeterR_parsed.root","FiftyMeterR_parsed.root","OneHundredMeterR_parsed.root","TwoHundredMeterR_parsed.root","FiveHundredMeterR_parsed.root"]
-#labels=["0.1m","4.0m","10.0m","30.0m","50.0m","100.0m","200.0m","500.0m"]
-
-#infNames=["FourMeterR_parsed.root","TenMeterR_parsed.root","ThirtyMeterR_parsed.root","FiftyMeterR_parsed.root","OneHundredMeterR_parsed.root","TwoHundredMeterR_parsed.root","FiveHundredMeterR_parsed.root"]
-#labels=["4.0m","10.0m","30.0m","50.0m","100.0m","200.0m","500.0m"]
-
-infNames=["VeffForAnalysis/parsedROOTfiles/ShadowParsedLogs.root","VeffForAnalysis/parsedROOTfiles/NoShadowParsedLogs.root","/pub/cpersich/ShelfMC/myshelfmc/ARIANNA_at_SP/parsedRootFiles/ARIANNA_at_SP_Single_WithShadow.root","/pub/cpersich/ShelfMC/myshelfmc/ARIANNA_at_SP/parsedRootFiles/ARIANNA_at_SP_Single_NoShadow.root"]
-labels=["Baseline Shadowing","Baseline No Shadowing","At SP Shadowing","At SP No Shadowing"]
-
-#infNames=eval(sys.argv[2])
-#labels=eval(sys.argv[3])
-
-inf={}
-nt={}
-Values={}
-VvsEAve={}
-VvsENuE={}
-VvsENuMu={}
-VvsENuTau={}
-ErrVsEAve={}
-ErrVsENuE={}
-ErrVsENuMu={}
-ErrVsENuTau={}
-
-for i in range(0,len(labels)):
-	inf[i]=ROOT.TFile(infNames[i])
-	nt[i]=inf[i].Get("nt")
-	Values[i]=[]
-	for ent in nt[i]:
-		E=ent.energy
-		VeffAve=ent.VeffAve
-		VeffNuE=ent.VeffNuE
-		VeffNuMu=ent.VeffNuMu
-		VeffNuTau=ent.VeffNuTau
-		WtNuE=ent.wtNuE
-		WtNuMu=ent.wtNuMu
-		WtNuTau=ent.wtNuTau
-		WtAve=(WtNuE+WtNuMu+WtNuTau)/3
-		Values[i].append((E,VeffAve,VeffNuE,VeffNuMu,VeffNuTau,WtAve,WtNuE,WtNuMu,WtNuTau))
-
-	Values[i].sort()
-
-	VvsEAve[i]={}
-	VvsENuE[i]={}
-	VvsENuMu[i]={}
-	VvsENuTau[i]={}
-	ErrVsEAve[i]={}
-	ErrVsENuE[i]={}
-	ErrVsENuMu[i]={}
-	ErrVsENuTau[i]={}
-
-	for (E,VeffAve,VeffNuE,VeffNuMu,VeffNuTau,WtAve,WtNuE,WtNuMu,WtNuTau) in Values[i]:
-		if E in VvsEAve[i]:
-			#First do a weighted sum of Veff for each energy
-			VvsEAve[i][E]+=VeffAve*WtAve
-			VvsENuE[i][E]+=VeffNuE*WtNuE
-			VvsENuMu[i][E]+=VeffNuMu*WtNuMu
-			VvsENuTau[i][E]+=VeffNuTau*WtNuTau
-			#First sum all the weights
-			ErrVsEAve[i][E]+=WtAve
-			ErrVsENuE[i][E]+=WtNuE
-			ErrVsENuMu[i][E]+=WtNuMu
-			ErrVsENuTau[i][E]+=WtNuTau
-		else:
-			#First do a weighted sum of Veff for each energy
-			VvsEAve[i][E]=VeffAve*WtAve
-			VvsENuE[i][E]=VeffNuE*WtNuE
-			VvsENuMu[i][E]=VeffNuMu*WtNuMu
-			VvsENuTau[i][E]=VeffNuTau*WtNuTau
-			#First sum all the weight
-			ErrVsEAve[i][E]=WtAve
-			ErrVsENuE[i][E]=WtNuE
-			ErrVsENuMu[i][E]=WtNuMu
-			ErrVsENuTau[i][E]=WtNuTau
-
-	for E in VvsEAve[i]:
-		if ErrVsEAve[i][E] !=0 :
-			#divide the weighted sum by the total weight to get the weighted average 
-			VvsEAve[i][E]=VvsEAve[i][E]/ErrVsEAve[i][E]
-			#StdErr = mean/sqrt(N)
-			ErrVsEAve[i][E]=VvsEAve[i][E]/n.sqrt(ErrVsEAve[i][E])
-		else:
-			VvsEAve[i][E]=0
-
-		if ErrVsENuE[i][E] != 0:
-			VvsENuE[i][E]=VvsENuE[i][E]/ErrVsENuE[i][E]
-			ErrVsENuE[i][E]=VvsENuE[i][E]/n.sqrt(ErrVsENuE[i][E])
-		else:
-			VvsENuE[i][E]=0
-
-		if ErrVsENuMu[i][E] != 0:
-			VvsENuMu[i][E]=VvsENuMu[i][E]/ErrVsENuMu[i][E]
-			ErrVsENuMu[i][E]=VvsENuMu[i][E]/n.sqrt(ErrVsENuMu[i][E])
-		else:
-			VvsENuMu[i][E]=0
-
-		if ErrVsENuTau[i][E] != 0:
-			VvsENuTau[i][E]=VvsENuTau[i][E]/ErrVsENuTau[i][E]
-			ErrVsENuTau[i][E]=VvsENuTau[i][E]/n.sqrt(ErrVsENuTau[i][E])
-		else:
-			VvsENuTau[i][E]=0
+NumFiles=len(labels)
 
 plt.figure(1,figsize=(12,9))
 plt.suptitle(title)
+ax1=plt.subplot(2,2,1)
+ax2=plt.subplot(2,2,2)
+ax3=plt.subplot(2,2,3)
+ax4=plt.subplot(2,2,4)
+#DArrays=[]
 
-plt.subplot(2,2,1)
-for i in range(0,len(labels)):
-	plt.errorbar(VvsEAve[i].keys(),VvsEAve[i].values(),yerr=ErrVsEAve[i].values(),label=labels[i],linestyle='None',marker='o')
-plt.xlabel("E")
-plt.ylabel("VeffAve (km3Str)")
-plt.yscale("log")
-plt.xscale("log")
-plt.gca().xaxis.grid(True)
-plt.gca().yaxis.grid(True)
+for i in range(0,NumFiles):
+	DArray=CalcVeffMutiLog(infNames[i])
+	#DArrays.append(DArray)
+	E, VeffAve, VeffNuE, VeffNuMu, VeffNuTau, VeffErrAve, VeffErrNuE, VeffErrNuMu, VeffErrNuTau = GetAllColumns(DArray)
 
-plt.subplot(2,2,2)
-for i in range(0,len(labels)):
-	plt.errorbar(VvsENuE[i].keys(),VvsENuE[i].values(),yerr=ErrVsENuE[i].values(),label=labels[i],linestyle='None',marker='o')
-plt.xlabel("E")
-plt.ylabel("VeffNuE (km3Str)")
-plt.yscale("log")
-plt.xscale("log")
-plt.gca().xaxis.grid(True)
-plt.gca().yaxis.grid(True)
+	ax1.errorbar(E,VeffAve,yerr=VeffErrAve,ls='None',marker='o',label=labels[i])
+	ax1.set_xlabel("E (eV)")
+	ax1.set_ylabel("VeffAve (km^3Str)")
+	ax1.set_xscale("log")
+	ax1.set_yscale("log")
+	ax1.grid()
 
-plt.subplot(2,2,3)
-for i in range(0,len(labels)):
-	plt.errorbar(VvsENuMu[i].keys(),VvsENuMu[i].values(),yerr=ErrVsENuMu[i].values(),label=labels[i],linestyle='None',marker='o')
-plt.xlabel("E (eV)")
-plt.ylabel("VeffNuMu (km3Str)")
-plt.yscale("log")
-plt.xscale("log")
-plt.gca().xaxis.grid(True)
-plt.gca().yaxis.grid(True)
+	ax2.errorbar(E,VeffNuE,yerr=VeffErrNuE,ls='None',marker='o',label=labels[i])
+	ax2.set_xlabel("E (eV)")
+	ax2.set_ylabel("VeffNuE (km^3Str)")
+	ax2.set_xscale("log")
+	ax2.set_yscale("log")
+	ax2.grid()
 
-plt.subplot(2,2,4)
-for i in range(0,len(labels)):
-	plt.errorbar(VvsENuTau[i].keys(),VvsENuTau[i].values(),yerr=ErrVsENuTau[i].values(),label=labels[i],linestyle='None',marker='o')
-plt.xlabel("E")
-plt.ylabel("VeffNuTau (km3Str)")
-plt.yscale("log")
-plt.xscale("log")
-plt.gca().xaxis.grid(True)
-plt.gca().yaxis.grid(True)
-plt.legend(loc='lower right')
-#plt.savefig(fileheader+".png",bbox_inches='tight')
+	ax3.errorbar(E,VeffNuMu,yerr=VeffErrNuMu,ls='None',marker='o',label=labels[i])
+	ax3.set_xlabel("E (eV)")
+	ax3.set_ylabel("VeffNuMu (km^3Str)")
+	ax3.set_xscale("log")
+	ax3.set_yscale("log")
+	ax3.grid()
+
+	ax4.errorbar(E,VeffNuTau,yerr=VeffErrNuTau,ls='None',marker='o',label=labels[i])
+	ax4.set_xlabel("E (eV)")
+	ax4.set_ylabel("VeffNuTau (km^3Str)")
+	ax4.set_xscale("log")
+	ax4.set_yscale("log")
+	ax4.grid()
+
+ax4.legend(loc=4)
 plt.show()
