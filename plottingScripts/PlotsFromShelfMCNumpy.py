119850d7df7b66d20b1499fb6628d63246aee540
persic <cpersich@uci.edu>
2018-04-25 23:56:50 -0700

    Added some handy numpy conversions

---
+++ plottingScripts/PlotsFromShelfMCNumpy.py
@@ -0,0 +1,47 @@
+import sys, os
+import numpy as np
+import matplotlib.pyplot as plt
+
+infn = sys.argv[1]
+
+Array = np.load(infn)
+
+X = Array['xyz'][:,0]
+Y = Array['xyz'][:,1]
+Z = Array['xyz'][:,2]
+Rho = np.sqrt(X*X+Y*Y)
+weight = Array['weight']
+Exp = Array['log10E']
+
+xBins = np.arange(0,4050,50)
+yBins = np.arange(0,2750,50)
+
+fig1, ax1 = plt.subplots()
+fig1.suptitle('Vertex Location')
+sc = ax1.hist2d(Rho,2700.0-Z,weights=weight,bins=[xBins,yBins])
+ax1.set_xlabel('Horizontal Distance (m)')
+ax1.set_ylabel('Depth (m)')
+ax1.set_ylim([2700,0])
+plt.colorbar(sc[3],ax=ax1)
+
+fig2, ax2 = plt.subplots()
+fig2.suptitle('Arrival Zenith')
+ax2.set_xlabel('Theta (deg)')
+ax2.hist(Array['arrivalTheta'],bins=np.arange(0,93,3))
+
+fig3, ax3 = plt.subplots()
+fig3.suptitle('Arrival Azimuth')
+ax3.set_xlabel('Phi (deg)')
+ax3.hist(Array['arrivalPhi'],bins=np.arange(0,370,10))
+
+fig4, ax4 = plt.subplots()
+fig4.suptitle('Voltage')
+ax4.hist(Array['vMaxTrig']*1e6,weights=weight,bins=60)
+ax4.set_xlabel('MaxV (micro-volt)')
+
+fig5, ax5 = plt.subplots()
+fig5.suptitle('Emission Angle')
+ax5.hist(Array['viewangle']-Array['coneAngle'],weights=weight,bins=60)
+ax5.set_xlabel('viewaAngle-coneAngle')
+
+plt.show()
--- utils/ShelfMC_ROOToutToNumpy.py
+++ utils/ShelfMC_ROOToutToNumpy.py
@@ -5,32 +5,31 @@ import numpy as np
 from EventListTools import *
 
 infn = sys.argv[1]
-EventListinfn = sys.argv[2]
-outfn = sys.argv[3]
+outfn = sys.argv[2]
 
 Fin = ROOT.TFile(infn)
 PAM = Fin.Get("PAM")
 
-EvtAndWeight =  root_numpy.tree2array(PAM,['ievt','weight'])
+ParamList = {'evtNum':'ievt',
+            'log10E':'Energy',
+            'flavor':'flavor',
+            'current':'mycurrent',
+            'xyz':'Posi_Int',
+            'inelasticity':'y',
+            'arrivalTheta':'theta_signal_atAT',
+            'arrivalPhi':'phi_nposnu2ST',
+            'nnu_theta':'theta_nu',
+            'nnu_phi':'phi_nu',
+            'viewangle':'viewangle_triggered',
+            'coneAngle':'changle',
+            'n_pol':'Fresnel_Pol_D',
+            'freq':'freq_my',
+            'EField':'vmmhz_my',
+            'vMaxTrig':'volte_allAT_mymax',
+            'weight':'weight'
+            }
 
-header = 'evid weight'
-typeFormat = ['%i','%f']
+ParameterArray =  root_numpy.tree2array(PAM,ParamList.values())
+ParameterArray.dtype.names = tuple(ParamList.keys())
 
-Events = ReadEventListARASimToListObject(EventListinfn)
-
-weights = np.zeros(len(Events))
-triggered = np.zeros(len(Events))
-
-OutArray = ARASimListArray(Events)
-
-for i, w in EvtAndWeight:
-    triggered[i] = 1
-    weights[i] = w
-
-OutArray = np.column_stack((OutArray,weights,triggered))
-
-header = 'evid nuflavorint nu_nubar pnu currentint posnu_r posnu_theta posnu_phi nnu_theta nnu_phi elast_y weight globalTrig'
-typeFormat = ['%i','%i','%i','%f','%i','%f','%f','%f','%f','%f','%f','%f','%i']
-
-
-np.savetxt(outfn,OutArray,header=header,fmt=typeFormat,delimiter=' ')
+np.save(outfn,ParameterArray)
